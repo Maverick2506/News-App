e921b0b27881583536db335e32756df9206d4b9e
Maverick2506 <122738707+Maverick2506@users.noreply.github.com>
2023-07-21 17:33:55 -0400

    Added links to categories, added author and publishing time and also added sources as badges

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,47 +2,32 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imgUrl, newsUrl } = this.props;
+    let { title, description, imgUrl, newsUrl, author, dateAndTime, source } =
+      this.props;
     return (
       <>
-        <div className="card my-3" style={{height: "32rem"}}>
+        <div className="card my-3" style={{ height: "40rem" }}>
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-dark"
+            style={{ left: "85%", zIndex: '1' }}
+          >
+            {source}
+          </span>
           <img
             src={imgUrl ? imgUrl : "/buffer.png"}
             className="card-img-top"
             alt="..."
           />
           <div className="card-body">
-            <h5
-              className="card-title"
-              style={{
-                maxWidth: "100%",
-                display: "-webkit-box",
-                WebkitBoxOrient: "vertical",
-                WebkitLineClamp: 2,
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {title}
-            </h5>
-            <p
-              className="card-text"
-              style={{
-                maxWidth: "100%",
-                display: "-webkit-box",
-                WebkitBoxOrient: "vertical",
-                WebkitLineClamp: 3,
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {description}
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(dateAndTime).toGMTString()}
+              </small>
             </p>
-            <a
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
